cfaa8c15d4f72717bc5528708edce4f0a934dd8b
André Silva <andreluiz.97@outlook.com>
2025-03-06 20:44:28 -0300

    feat: change gallery title

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -33,7 +33,7 @@ export function GallerySection() {
 
   return (
     <section>
-      <Text variant="h2" className="mb-8 text-amber-500">Gallery</Text>
+      <Text variant="h2" className="mb-8 text-amber-500">Galeria</Text>
       <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 2xl:grid-cols-8 gap-4">
         { images.map((image, index) => (
           <li
