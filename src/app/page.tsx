7d0b9c54a50c15e8641ca359ba2d73a8678a4aa3
André Silva <andreluiz.97@outlook.com>
2025-03-05 22:03:13 -0300

    feat: improve gap between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { GallerySection } from "@/components/GallerySection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-16 px-4 py-8 sm:p-8 md:p-12 lg:p-16 xl:p-24 text-center">
+    <main className="flex flex-col gap-8 md:gap-12 lg:gap-16 px-4 py-8 sm:p-8 md:p-12 lg:p-16 xl:px-24 text-center">
       <IntroSection />
       <GallerySection />
     </main>
