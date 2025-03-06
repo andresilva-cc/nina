25c646c9d0d744393d6f51bc1dd7b1e935aa4bac
André Silva <andreluiz.97@outlook.com>
2025-03-06 20:38:30 -0300

    feat: change texts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="text-center pt-8 md:pt-12 lg:pt-16">
       <hr className="mb-8 border-gray-700" />
       <Text>
-        Developed by <a
+        Desenvolvido por <a
           href="https://andresilva.cc/"
           target="_blank"
           className="underline text-amber-500 hover:opacity-80 active:opacity-60 transition-opacity"
--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -4,8 +4,12 @@ export function IntroSection() {
   return (
     <section>
       <Text variant="h1" className="mb-8 text-amber-500">Nina</Text>
-      <Text className="max-w-prose mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-      <Text variant="handwritten" className="mt-8">Lorem ipsum dolor sit amet</Text>
+      <div className="flex flex-col gap-4 max-w-prose mx-auto">
+        <Text>Nina foi resgatada em 27 de outubro de 2014. Ela estava perdida na rua quando meu pai, ao sair inesperadamente do trabalho no meio da manhã, a encontrou. Pequena, assustada e sem rumo, foi acolhida por ele e levada para casa. A primeira foto da galeria mostra Nina em seu primeiro dia no novo lar, ainda um pouco desconfiada.</Text>
+        <Text>Desde então, ela se tornou uma parte essencial da nossa família, sempre companheira e trazendo alegria para todos. Costumava esperar no portão quando alguém saía de casa e nos recebia com muito carinho na volta. Foram anos de carinho, companheirismo e lembranças que ficarão para sempre.</Text>
+        <Text>Nina viveu pouco mais de 10 anos, enfrentando um câncer com a mesma força que sempre teve. Infelizmente, na manhã de 27 de fevereiro de 2025, ela nos deixou.</Text>
+        <Text variant="handwritten" className="mt-4">Para sempre em nossos corações</Text>
+      </div>
     </section>
   )
 }
